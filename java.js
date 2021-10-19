f85dbb7b932539c8587aa3ef0d0f61022360dfbc
Jason <41246503+M0ISE@users.noreply.github.com>
2021-10-19 23:26:44 +1100

    Resetting up files from september

---
+++ java.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+var moonCake = document.getElementsById("jsonData")
+
+console.log(JASON);
+
+var arr = [ "John", "Peter", "Sally", "Jane" ];
+
+var myJSON = JSON.stringify(moonCake);
+
+document.getElementById("demo").innerHTML = myJSON;
+
+};
